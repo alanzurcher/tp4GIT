0859c187a3bb6c6485bbfec71ed16c03d9480834
alann1889 <zurcher1889@gmail.com>
2020-09-07 18:24:45 -0300

    Tercer commit, Programa Finalizado

--- boleto.py
+++ boleto.py
@@ -10,8 +10,20 @@ p=0
 tr=0
 x=input("ingrese C para comenzar, F para finalizar")
 #Cargamos el comienzo y si el pasajero paga medio boleto o entero
-
-
-
+while x=="C":
+    t=input("ingrese MB para medio boleto, para boleto completo presione B")
+    c=c+1
+    if t=="MB":
+        pb=92.50/2
+        print("El pasajero debe pagar",pb,)
+    else:
+        t=="B"
+        pb=92.50
+        print("el pasajero debe pagar",pb,)
+    l1.append(pb)
+    x=input("ingrese C para continuar, F para finalizar")
 
 #Mostramos el total recaudado y la cantidad de pasajeros
+print("el total recaudado fue de ",sum(l1),)
+print("la cantidad de pasajeros que subieron fueron ",c,)
+
