c2cae710e3dd44ba8f3defd82bf747f734d85b04
alann1889 <zurcher1889@gmail.com>
2020-09-07 18:19:38 -0300

    Segundo commit, declaracion de variables

--- boleto.py
+++ boleto.py
@@ -1,4 +1,17 @@
 
-boleto.py trata de contabilizar los boletos y las cantidad de personas
-ademas de tener la opcion de poder pagar medio boleto o entero
+#boleto.py trata de contabilizar los boletos y las cantidad de personas
+#ademas de tener la opcion de poder pagar medio boleto o entero
 
+#inicializamos las variables
+c=0
+l1=[]
+pb=0
+p=0
+tr=0
+x=input("ingrese C para comenzar, F para finalizar")
+#Cargamos el comienzo y si el pasajero paga medio boleto o entero
+
+
+
+
+#Mostramos el total recaudado y la cantidad de pasajeros
